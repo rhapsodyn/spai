d7d054b06d5b08a357b2947cda4afe0e87ebb151
tassadarliu <rhapsodyn@gmail.com>
2013-12-25 21:45:27 +0800

    pass 80 tests
    TODO: thanable chain
    TODO: callback queue

--- src/Spai.js
+++ src/Spai.js
@@ -1,48 +1,112 @@
 (function (window) {
 	//latter overwrite former
 	var Spai =  window.Spai || {};
+
 	var isFunction = function (value) {
 		return typeof value === "function";
 	};
+	var allStates = {
+		pending: 0,
+		fulfilled: 1,
+		rejected: 2
+	};
+	var mutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	var callbackQueue = [];
+	var invoke = function () {
+		for (var i = 0; i < callbackQueue.length; i++) {
+			var tuple = callbackQueue[i],
+				fn = tuple[0],
+				arg = tuple[1];
 
-	Spai.promise = function(fn) {
+			try {
+				fn(arg);
+			}
+			catch (e) {
+
+			}
+		}
+		callbackQueue = [];
+	};
+	//steal form rsvp.js
+	var flush = (function () {
+		//node env
+		if (typeof process !== 'undefined' && toString.call(process) === '[object process]') {
+			return function() {
+				process.nextTick(invoke);
+			};
+		//modern browser
+		} else if (mutationObserver) {
+			//todo
+		//old browser
+		} else {
+			//todo
+		}
+	})();
+	var asycnInvoke = function (fn, arg) {
+		callbackQueue.push([fn, arg]);
+		flush();
+	};
+
+	Spai.promise = function (fn) {
 		var promise = {};
-		promise.state = "pending";
+		//private properties
+		var _state = allStates.pending,
+			_onFulfilledQueue = [],
+			_onRejectedQueue = [],
+			_resolveValue,
+			_rejectedReason;
 		promise.then = function (onFulfilled, onRejected) {
+			var returnValue;
+			if (_state === allStates.fulfilled && isFunction(onFulfilled)) {
+				asycnInvoke(onFulfilled, _resolveValue);
+			}
+			else if (_state === allStates.rejected && isFunction(onRejected)) {
+				asycnInvoke(onRejected, _rejectedReason);
+			}
+			else {
+				_onFulfilledQueue.push(onFulfilled);
+				_onRejectedQueue.push(onRejected);
+			}	
+
+			//THIS LINE is A BUG!
+			return promise;
 		};
 
 		var resolve = function (value) {
-			if (promise.state === "pending") {
-				promise.state = "fulfilled";
+			if (_state === allStates.pending) {
+				_state = allStates.fulfilled;
+				_resolveValue = value;
 			}
 			else {
 				return;
 			}
 
-			if (promise.onFulfilled) {
-				promise.onFulfilled(value);
+			for (var i = 0; i < _onFulfilledQueue.length; i++) {
+				asycnInvoke(_onFulfilledQueue[i], _resolveValue);
 			}
 		};
-		var reject = function  (reason) {
-			if (promise.state === "pending") {
-				promise.state = "rejected";
+		var reject = function (reason) {
+			if (_state === allStates.pending) {
+				_state = allStates.rejected;
+				_rejectedReason = reason;
 			}
 			else {
 				return;
 			}
 
-			if (promise.onRejected) {
-				promise.onRejected(reason);
+			for (var i = 0; i < _onRejectedQueue.length; i++) {
+				asycnInvoke(_onRejectedQueue[i], _rejectedReason);
 			}
 		};
 
 		if (isFunction(fn)) {
-			fn.call(promise, resolve, reject);
+			fn.call(this, resolve, reject);
 		}
 		return promise;
 	};
 
-	Spai.defer = function() {
+	//meets test suite's need
+	Spai.defer = function () {
 		var deferred = {};
 		deferred.promise = Spai.promise(function (resolve, reject) {
 			deferred.resolve = resolve;
